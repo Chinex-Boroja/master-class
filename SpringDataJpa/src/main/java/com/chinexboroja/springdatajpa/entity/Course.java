c1ee6ed6ceac1f79c43a15574c88141bd1302f75
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-05-16 09:34:11 +0100

    Chore: JPA relationship: One to One

---
+++ SpringDataJpa/src/main/java/com/chinexboroja/springdatajpa/entity/Course.java
@@ -0,0 +1,15 @@
+package com.chinexboroja.springdatajpa.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Course {
+}
---
+++ SpringDataJpa/src/main/java/com/chinexboroja/springdatajpa/entity/CourseMaterial.java
@@ -0,0 +1,15 @@
+package com.chinexboroja.springdatajpa.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CourseMaterial {
+}
