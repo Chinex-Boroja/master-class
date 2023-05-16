68924b18faa9213a3472e95f8a1a61ffa98052a8
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-05-16 09:54:41 +0100

    Chore: Mapped course material to course

--- SpringDataJpa/src/main/java/com/chinexboroja/springdatajpa/entity/CourseMaterial.java
+++ SpringDataJpa/src/main/java/com/chinexboroja/springdatajpa/entity/CourseMaterial.java
@@ -1,6 +1,6 @@
 package com.chinexboroja.springdatajpa.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,4 +12,24 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class CourseMaterial {
+
+    @Id
+    @SequenceGenerator(
+            name = "course_material_sequence",
+            sequenceName = "course_material_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "course_material_sequence"
+    )
+    private Long courseMaterialId;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(
+            name = "course_id",
+            referencedColumnName = "courseId"
+    )
+    private Course course;
 }
