d72ef13d09a4072c422816666ea104b6e863b49c
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-07-03 16:11:21 +0100

    Chore: resend verification token

--- Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/controller/RegistrationController.java
+++ Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/controller/RegistrationController.java
@@ -2,13 +2,16 @@ package com.chinexboroja.SpringSecurityClient.controller;
 
 import com.chinexboroja.SpringSecurityClient.dto.UserDTO;
 import com.chinexboroja.SpringSecurityClient.entity.User;
+import com.chinexboroja.SpringSecurityClient.entity.VerificationToken;
 import com.chinexboroja.SpringSecurityClient.event.RegistrationCompleteEvent;
 import com.chinexboroja.SpringSecurityClient.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class RegistrationController {
 
@@ -33,6 +36,21 @@ public class RegistrationController {
         }
         return "Bad User";
      }
+
+     @GetMapping("/resendVerifyToken")
+     public String resendVerificationToken(@RequestParam("token") String oldToken, HttpServletRequest request) {
+
+         VerificationToken verificationToken = userService.generateVerificationToken(oldToken);
+         User user = verificationToken.getUser();
+         resendVerificationTokenMail(user, applicationUrl(request), verificationToken);
+         return "Verification Link Sent";
+     }
+
+    private void resendVerificationTokenMail(User user, String applicationUrl, VerificationToken verificationToken) {
+
+        String url = applicationUrl + "/verifyRegistration?token=" + verificationToken.getToken();
+        log.info("Click the link to verify your account: {}", url);
+    }
 
     private String applicationUrl(HttpServletRequest request) {
         return "http://" + request.getServerName() + ":" + request.getServerPort() + request.getContextPath();
--- Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/service/UserService.java
+++ Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/service/UserService.java
@@ -2,6 +2,7 @@ package com.chinexboroja.SpringSecurityClient.service;
 
 import com.chinexboroja.SpringSecurityClient.dto.UserDTO;
 import com.chinexboroja.SpringSecurityClient.entity.User;
+import com.chinexboroja.SpringSecurityClient.entity.VerificationToken;
 
 public interface UserService {
     User registerUser(UserDTO userDTO);
@@ -9,4 +10,6 @@ public interface UserService {
     void saveVerificationTokenForUser(String token, User user);
 
     String validateVerificationToken(String token);
+
+    VerificationToken generateVerificationToken(String oldToken);
 }
