66fdffd73ddbf3d0ea77b7356857b75f5b182db7
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-07-18 09:54:09 +0100

    Chore: Simple JDBC demo

--- Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/config/WebSecurityConfig.java
+++ Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.chinexboroja.SpringSecurityClient.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -30,7 +31,12 @@ public class WebSecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(request ->
                         request.requestMatchers(WHITE_LIST_URLS).permitAll()
-                                .anyRequest().permitAll());
+                                .anyRequest().permitAll().
+                                requestMatchers("/api/**")
+                                .authenticated())
+                                .oauth2Login(oauth2login ->
+                                        oauth2login.loginPage("/oauth2/authorization/api-client-oidc"))
+                                .oauth2Client(Customizer.withDefaults());
         return http.build();
     }
 }
--- Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/controller/HelloController.java
+++ Spring-Security/Spring-Security-Client/src/main/java/com/chinexboroja/SpringSecurityClient/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @PostMapping("/hello")
+    @PostMapping("/api/hello")
     public String registerUser() {
         return "Hello Chinedu";
     }
