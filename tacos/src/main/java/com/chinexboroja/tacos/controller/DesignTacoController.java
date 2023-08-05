f9513fb5c58056d7ed0b4c578f33c1e45cd058e4
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-08-05 01:16:02 +0100

    Chore: rendered design.html page

--- tacos/src/main/java/com/chinexboroja/tacos/controller/DesignTacoController.java
+++ tacos/src/main/java/com/chinexboroja/tacos/controller/DesignTacoController.java
@@ -6,10 +6,7 @@ import com.chinexboroja.tacos.domain.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,7 +14,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Controller
-@RequestMapping("/")
+@RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
 
