483fe858084793213d4fbb219f67efb55d982bd1
Ihedioha Chinedu Innocent <ihediohachinedu21@gmail.com>
2023-09-05 18:13:52 +0100

    Chore: Added config for MongoDB

--- tacos/src/main/java/com/chinexboroja/tacos/domain/Ingredient.java
+++ tacos/src/main/java/com/chinexboroja/tacos/domain/Ingredient.java
@@ -1,17 +1,21 @@
 package com.chinexboroja.tacos.domain;
 
 import lombok.*;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @RequiredArgsConstructor
-@Table("ingredients")
+//@Table("ingredients") // Cassandra mapping to Table
+@Document(collection = "ingredients") // Mongo
 @NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
 
-    @PrimaryKey
+//    @PrimaryKey
+    @Id
     private final String id;
     private final String name;
     private final Type type;
